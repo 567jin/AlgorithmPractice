d48952b5e473eeab79f7357b893d0db459471482
jin567 <jinzhiyuan567@gmail.com>
2024-01-05 11:17:21 +0800

    add:单向链表

---
+++ src/utils/ListNode.java
@@ -0,0 +1,24 @@
+package utils;
+
+/**
+ * @author jzy
+ * @since 2024-01-05 11:15:54
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+}
