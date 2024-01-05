40365df7007b57dbf6e7c0844b292f434e5c1c9b
jin567 <jinzhiyuan567@gmail.com>
2024-01-05 14:09:04 +0800

    add: 链表操作

--- src/utils/ListNode.java
+++ src/utils/ListNode.java
@@ -6,19 +6,62 @@ package utils;
  */
 public class ListNode {
 
-    int val;
-    ListNode next;
+    private int val;
+    public ListNode next;
 
     ListNode() {
     }
 
-    ListNode(int val) {
+    public ListNode(int val) {
         this.val = val;
     }
 
-    ListNode(int val, ListNode next) {
+    public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
     }
 
+    public static ListNode genList(int[] nums) {
+        ListNode head = new ListNode(nums[0]);
+        ListNode cur = head;
+        for (int i = 1; i < nums.length; i++) {
+            ListNode newNode = new ListNode(nums[i]);
+            cur.next = newNode;
+            cur = cur.next;
+        }
+
+        return head;
+    }
+
+    /**
+     * 打印链表
+     *
+     * @param head 链表头结点
+     */
+    public static void printList(ListNode head) {
+        ListNode cur = head;
+        while (cur != null) {
+            System.out.print(cur.getVal() + ", ");
+            cur = cur.next;
+        }
+        System.out.println();
+
+    }
+
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
 }
